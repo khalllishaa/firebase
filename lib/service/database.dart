31e65fe64417610e3f84934f6482f6127aa09bf0
Zaskia1 <ameliazaskia38@gmail.com>
2025-01-13 10:30:57 +0700

    feat: [ZA] firebase crud

---
+++ lib/service/database.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseMethods{
+
+  //CREATE
+  Future addEmployeeDetails(Map<String, dynamic> employeeInfoMap, String id)async{
+  return await FirebaseFirestore.instance
+      .collection("employee")
+      .doc(id)
+      .set(employeeInfoMap);
+  }
+
+  //READ
+  Future<Stream<QuerySnapshot>> getEmployeeDetails()async{
+    return await FirebaseFirestore.instance.collection("employee").snapshots();
+  }
+
+
+  //UPDATE
+  Future updateEmployeeDetail(String id, Map <String, dynamic> updateInfo)async{
+    return await FirebaseFirestore.instance.collection("employee").doc(id).update(updateInfo);
+  }
+
+  //DELETE
+  Future deleteEmployeeDetail(String id) async {
+    return await FirebaseFirestore.instance.collection("employee").doc(id).delete();
+  }
+
+}
